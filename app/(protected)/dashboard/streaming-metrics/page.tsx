e6172cb4d7b1359cadb488f6defdacffc398f1ee
rkmiller131 <rkmiller131@gmail.com>
2025-08-26 13:13:49 -0700

    added cloud streaming mock graphs

--- app/(protected)/dashboard/streaming-metrics/page.tsx
+++ app/(protected)/dashboard/streaming-metrics/page.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
+
 export default function StreamingMetrics() {
   return (
-    <div>
-      <span>Cloud Streaming Metrics coming soon:</span>
-      <ul className="mt-6">
-        <li>Streaming Quality</li>
-        <li>Map-based Geo-Performance</li>
-        <li>Concurrent Players</li>
-      </ul>
+    <div className="flex flex-col gap-4">
+      <div className="cursor-not-allowed flex gap-2 items-start border-b border-gray-500 pb-2 w-max">
+        <span className="">
+          Cyberpunk 2077
+        </span>
+        <Image
+          src="/icons/down-caret.png"
+          alt="Dropdown icon"
+          width={12}
+          height={12}
+          className="pt-1"
+        />
+      </div>
+
+      <Image
+        src="/images/CloudMetricsGraphs.svg"
+        alt="Mock User Engagement Graph"
+        width={855}
+        height={720}
+      />
     </div>
   )
 }
